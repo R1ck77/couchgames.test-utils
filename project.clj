e60d5086c89ec390cc16a92ed22db3dc3bf52233
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2019-01-26 20:00:45 +0100

    Increase the version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject couchgames/test-utils "0.1.1"
+(defproject couchgames/test-utils "0.1.2-SNAPSHOT"
   :description "Mixed bag of testing functions and macros"
   :url "https://github.com/R1ck77/couchgames.test-utils"
   :license {:name "CC Attribution 4.0 International with exception for binary distribution"
